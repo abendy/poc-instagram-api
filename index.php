66b9ed17e32083e33a1c71c5de0f4b4e8a3958e0
Allan Bendy <allan.bendy@gmail.com>
2017-02-07 23:14:20 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="en" itemscope itemtype="http://schema.org/Product">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Login - poc-instagram-api</title>
+  </head>
+  <body>
+    <?php
+    require_once(__DIR__ . '/config.php');
+    ?>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">window.jQuery || document.write('<script src="/local/jquery.min.js"><\/script>')</script>
+  </body>
+</html>
