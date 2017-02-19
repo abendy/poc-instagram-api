8b46e021ecaa0bb3a6728270db3015da98ad3604
Allan Bendy <allan.bendy@gmail.com>
2017-02-18 21:31:52 -0500

    moves css to file

--- web/index.php
+++ web/index.php
@@ -10,10 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="./vendor/material-design-lite/material.min.css">
-    <style>
-    body { margin: 1em; }
-    a { margin-bottom: 1em !important; }
-    </style>
+    <link rel="stylesheet" href="./assets/style.css">
   </head>
   <body>
     <?php
