87be26cdbefb20101bc796aeebbefd3da65a0293
Allan Bendy <allan.bendy@gmail.com>
2017-02-24 13:40:01 -0500

    adds bootstrap

--- web/index.php
+++ web/index.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="vendor/material-design-lite/material.min.css">
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap-theme.min.css">
     <link rel="stylesheet" href="assets/style.css">
   </head>
   <body>
