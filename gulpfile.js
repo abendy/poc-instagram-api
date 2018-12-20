517a22a8c20920d445989a17ade59ebba05d98d3
Allan Bendy <allan.bendy@gmail.com>
2018-12-21 10:33:38 +1100

    restructure + sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,54 @@
 'use strict';
 
+const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const del = require('del');
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
-const sourceFiles = [
-    'node_modules/material-design-lite/dist/material.min.css',
-    'node_modules/material-design-lite/dist/material.min.js'
-];
+const src = 'web/assets/src/';
+const dist = 'web/assets/dist/';
 
-const destination = 'web/assets/libs/';
-const dist = 'web/dist/';
+sass.compiler = require('node-sass');
 
 //////////////////////////////
 // Begin Gulp Tasks
 //////////////////////////////
 
-function clean() {
-    return del(destination);
-}
-
-function copy() {
-    return gulp
-        .src(sourceFiles)
-        .pipe(gulp.dest(destination));
+function clean(cb) {
+    return del([dist], cb);
 }
 
 function scripts() {
-    return gulp.src(destination + '*.js')
-        .pipe(concat('main.js'))
+    return gulp.src([
+            'node_modules/material-design-lite/dist/material.js'
+        ])
         .pipe(uglify())
+        .pipe(concat('main.js'))
         .pipe(gulp.dest(dist));
 }
 
 function styles() {
-    return gulp.src([destination + '*.css', 'web/assets/style.css'])
-        .pipe(concat('main.css'))
+    return gulp.src([
+            'node_modules/material-design-lite/src/material-design-lite.scss',
+            src + '*.scss'
+        ])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(cleanCSS())
-        .pipe(gulp.dest(dist));
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest(dist))
 }
 
-const build = gulp.parallel(clean, copy, scripts, styles);
+function watch() {
+    return gulp.watch(src + '*.scss', ['styles']);
+}
 
-exports.build = build;
+const build = gulp.parallel(clean, scripts, styles);
+
 exports.default = build;
+exports.build = build;
 exports.clean = clean;
+exports.watch = watch;
--- web/index.php
+++ web/index.php
@@ -6,12 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 
         <title>poc-instagram-api</title>
+
         <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
 
-        <!-- //TODO use SASS to import the external stylesheets -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <link rel="stylesheet" href="dist/main.css">
+        <link rel="stylesheet" href="assets/dist/main.css">
     </head>
     <body>
         <main>
@@ -68,6 +66,6 @@
         ?>
         </main>
 
-        <script defer src="dist/main.js"></script>
+        <script defer src="assets/dist/main.js"></script>
     </body>
 </html>
